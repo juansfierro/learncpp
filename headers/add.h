52ba99cc99a93a30bae12e674276de8898ec31d2
juansfierro <j.fierro.pr@gmail.com>
2026-02-12 10:05:10 -0400

    Practice with Headers.

---
+++ headers/add.h
@@ -0,0 +1,3 @@
+//Missing Header Guard
+
+int add(int x, int y);
---
+++ headers/main.cpp
@@ -0,0 +1,8 @@
+#include "add.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "The sum of 3 and 4: " << add(3,4) << std::endl;
+	return 0;
+}
